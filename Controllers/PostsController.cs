2abdc4adc63ff0224b756bbb75b078a88cd7eb2a
Łukasz Drabek <lpdrabek@gmail.com>
2018-01-23 20:16:50 +0100

    Initial commit.

---
+++ Controllers/PostsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BlogApp.Models;
+
+namespace BlogApp.Controllers
+{
+    public class PostsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Show(int id)
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            ViewData["Message"] = "Your add page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
